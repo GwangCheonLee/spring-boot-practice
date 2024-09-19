b88249a13002c9543ba46622ad9821eb5fe81929
GwangCheonLee <raitmid56@gmail.com>
2024-09-19 22:56:44 +0900

    chore: Add environment-specific configuration files

--- src/main/java/com/example/springbootpractice/SpringBootPracticeApplication.java
+++ src/main/java/com/example/springbootpractice/SpringBootPracticeApplication.java
@@ -2,13 +2,14 @@ package com.example.springbootpractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+import java.util.TimeZone;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class SpringBootPracticeApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(SpringBootPracticeApplication.class, args);
     }
 
