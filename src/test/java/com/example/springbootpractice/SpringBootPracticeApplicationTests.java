fdbf8336b5c832214022ebf4c0534dbe4fbd326c
GwangCheonLee <raitmid56@gmail.com>
2024-09-19 20:46:27 +0900

    chore: project init

---
+++ src/test/java/com/example/springbootpractice/SpringBootPracticeApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.springbootpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // TODO document why this method is empty
+    }
+
+}
